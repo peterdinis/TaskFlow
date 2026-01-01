5110c64ebf6699190212e8fe596e01258da16695
peterdinis <petter1@pobox.sk>
2026-01-01 14:04:14 +0100

    feat: Update Dashboard

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -5,6 +5,7 @@ import { createFileRoute } from "@tanstack/react-router";
 export const Route = createFileRoute("/dashboard")({
 	component: RouteComponent,
 	pendingComponent: AnimatedLoader,
+	beforeLoad: () => <AnimatedLoader />
 });
 
 function RouteComponent() {
